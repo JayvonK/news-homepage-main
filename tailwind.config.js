767604c4ff59f9154e9be68168966241eef714a6
JayvonK <jaykhuth@gmail.com>
2024-02-06 02:02:54 -0800

    responsiveness done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     "./node_modules/flowbite/**/*.js",
     "./*.{html,js}"],
   theme: {
+    colors: {
+      redd: 'hsl(5, 85%, 63%)',
+      softO: 'hsl(35, 77%, 62%)',
+      darkBlu: 'hsl(240, 100%, 5%)',
+    },
     extend: {},
   },
   plugins: [
